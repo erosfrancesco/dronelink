575a0d3f2378ba0565c69be4db5d749e3f690cd2
erosfrancesco <eros.francesco.francesco@gmail.com>
2023-12-21 19:00:41 +0100

    HexagonBox flexible

--- src/components/HexagonBox.js
+++ src/components/HexagonBox.js
@@ -9,44 +9,60 @@ export const HexagonBox = (...args) => {
     ...args
   );
 
-  const height = "5em";
-  const width = "10em";
-
   return div(
     {
-      class: "content-background",
-      style: () => "height:" + height + ";width:" + width,
+      class: () => "deco-box" + (componentClass ? " " + componentClass : ""),
+      ...otherProps,
+      // TODO: - Animation
     },
-
-    div({
-      class: "deco-border deco-gradient-corner-1", // top left
-    }),
-    div({
-      class: "deco-border border-top deco-gradient-ver", // top
-    }),
-    div({
-      class: "deco-border deco-gradient-corner-2", // top right
-    }),
-    div({
-      class: "deco-border border-left deco-gradient-hor", // left
-    }),
-    div({
-      class: "deco-border border-bottom-left deco-gradient-corner-3", // left bottom
-    }),
-    div({
-      class: "deco-border border-bottom deco-gradient-ver", // bottom
-    }),
-    div({
-      class: "deco-border border-right deco-gradient-hor", // right
-    }),
-    div({
-      class: "deco-border border-bottom-right deco-gradient-corner-4", // bottom right
-    }),
     div(
       {
-        class: "deco-border-content",
+        class: "deco-box-content",
       },
-      ...childs
+      div({
+        class: "deco-border deco-gradient-corner-1", // top left
+      }),
+      div({
+        class: "deco-border border-top deco-gradient-ver", // top
+      }),
+      div({
+        class: "deco-border deco-gradient-corner-2", // top right
+      })
+    ),
+
+    div(
+      {
+        class: "deco-box-content",
+        style: "flex: 1 !important;",
+      },
+      div({
+        class: "deco-border border-left deco-gradient-hor", // left
+      }),
+      div(
+        {
+          class: "deco-border-content",
+        },
+        ...childs
+      ),
+      div({
+        class: "deco-border border-right deco-gradient-hor", // right
+      })
+    ),
+
+    div(
+      {
+        class: "deco-box-content",
+      },
+      div({
+        class: "deco-border border-bottom-left deco-gradient-corner-3", // left bottom
+      }),
+      div({
+        class: "deco-border border-bottom deco-gradient-ver", // bottom
+      }),
+
+      div({
+        class: "deco-border border-bottom-right deco-gradient-corner-4", // bottom right
+      })
     )
   );
 };
--- src/screens/Develop/index.js
+++ src/screens/Develop/index.js
@@ -26,7 +26,7 @@ export const Develop = () => {
     Status: systemStatus,
   };
 
-  return HexagonBox(span("Hello world"));
+  return HexagonBox({ style: 'width: 20em; height: 10em;' }, span("Hello world"));
 
   /*
   return VerticalLayout({ class: "Home-Section" }, () =>
