e89b1283e79eccee4544e6a3a9dda89c417f22fb
erosfrancesco <eros.francesco.francesco@gmail.com>
2023-12-22 13:44:24 +0100

    van property parser in case of string

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,10 +1,14 @@
 export const VanComponentArgsParser = (...args) => {
   const [first, ...childs] = args || [];
 
+  if (typeof first === "string") {
+    return { childs: [first], otherProps: {} };
+  }
+
   // if first argument is a dom node or a function, then it's a child
   // if not, it's a prop
   if (first?.nodeName || typeof first === "function") {
-    return { childs: [first, ...childs] };
+    return { childs: [first, ...childs], otherProps: {} };
   }
 
   const { class: componentClass, ...otherProps } = first || {};
