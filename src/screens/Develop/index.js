08b15abdd65f5db49906444076529804fa19c8b6
erosfrancesco <eros.francesco.francesco@gmail.com>
2024-01-18 16:27:51 +0100

    xml containing param defs

--- src/screens/Develop/index.js
+++ src/screens/Develop/index.js
@@ -48,7 +48,46 @@ const setupWithMock = () => {
 
 export const Develop = () => {
   // setupWithMock();
-  return ParameterWatcher({ paramId: "ATC_ANG_YAW_P" });
+  return div(
+    ParameterWatcher({ paramId: "ATC_RAT_YAW_P" }),
+    ParameterWatcher({ paramId: "ATC_RAT_YAW_I" }), // 0.07999999821186066
+    ParameterWatcher({ paramId: "ATC_RAT_YAW_D" })
+  );
 };
 
 export default Develop;
+/**
+Roll angular P gain ATC_ANG_RLL_P
+Roll rate P, I and D gains ATC_RAT_RLL_P, ATC_RAT_RLL_I, ATC_RAT_RLL_D
+Roll max acceleration ATC_ACCEL_R_MAX
+
+Pitch angular P gain ATC_ANG_PIT_P
+Pitch rate P, I and D gains ATC_RAT_PIT_P, ATC_RAT_PIT_I, ATC_RAT_PIT_D
+Pitch max acceleration ATC_ACCEL_P_MAX
+
+Yaw angular P gain ATC_ANG_YAW_P
+Yaw rate P, I gain ATC_RAT_YAW_P, ATC_RAT_YAW_I, ATC_RAT_YAW_D
+Yaw rate D gain ATC_RAT_YAW_D (in AC4.4 and higher)
+
+Yaw rate filter ATC_RAT_YAW_FLTT, ATC_RAT_YAW_FLTE (in AC3.6: ATC_RAT_YAW_FLT)
+
+Yaw max acceleration ATC_ACCEL_Y_MAX
+ */
+
+/**
+Drone reacts slowly and hovers bad or unstable (eg dances, wobbles)
+Increase P-value and decrease D value. Maybe decrease I value. 
+The P value must in no case be higher than the above-determined threshold.
+
+During hover the drone drifts
+Increase the I term and possibly P-value.
+
+Oscillation during fast flights
+I-value too high or too low - depending on the drone.
+
+Drone oscillates slightly with disturbances (eg nudge or gust of wind)
+D value down
+
+Drone reacts too slowly to disturbances (eg nudge or gust of wind)
+Increase D-value and possibly P-value.
+ */
--- src/ui/ParamWatcher.widget.js
+++ src/ui/ParamWatcher.widget.js
@@ -9,7 +9,60 @@ import {
   HorizontalLayout,
   VerticalLayout,
 } from "../components/index.js";
+
+// TODO: - WRAPPER
+function loadXMLDoc() {
+  const xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = () => {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      console.log("Got it", xmlhttp.responseXML);
+      const paramFile = xmlhttp.responseXML.getElementsByTagName("paramfile");
+      // paramfile => vehicle => parameters
+      const parameters = paramFile[0].childNodes[1].children[0];
+
+      const {
+        childNodes,
+        textContent,
+        tagName,
+        attributes: attributeEnum,
+      } = parameters;
+      childNodes.forEach((node) => {
+        const {
+          textContent,
+          tagName,
+          childNodes,
+          attributes: attributeEnum,
+        } = node;
+
+        Object.keys(attributeEnum || {}).forEach((attributeName) => {
+          const attribute = attributeEnum[attributeName];
+          const { nodeName, nodeValue } = attribute;
+          console.log(
+            attributeEnum.humanName.nodeValue,
+            ":",
+            nodeName,
+            "=",
+            nodeValue
+          );
+        });
+      });
+    }
+    /**
+     * eg:
+  Takeoff Check RPM maximum : humanName = Takeoff Check RPM maximum
+  Takeoff Check RPM maximum : name = ArduCopter:TKOFF_RPM_MAX
+  Takeoff Check RPM maximum : documentation = Takeoff is not permitted until motors report no more than this RPM.  Set to zero to disable check
+  Takeoff Check RPM maximum : user = Standard
+     */
+  };
+  xmlhttp.open("GET", "apm.pdef.xml", true);
+  xmlhttp.send();
+}
+loadXMLDoc();
+
 const { div, span, input } = van.tags;
+
+// TODO: - Button for copying previous values
 
 //
 const writeParameter = (packet) => {
